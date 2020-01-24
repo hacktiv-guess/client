2e91c1c28c837768218d679a9ad7d9cc147da668
sukmabrahmantya <brahmantyasukma@gmail.com>
2020-01-24 16:37:36 +0700

    done

--- src/components/RoomCard.vue
+++ src/components/RoomCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <b-card no-body class='mb-3 border-primary'>
+    <b-card no-body class='mb-3'>
       <b-row no-gutters class='bg-secondary'>
-        <b-col md="5" class='offset-md-1'>
-          <b-card-body :title='roomDetail.name' class='p-2' style='min-height: 120px; color:white'>
+        <b-col md="5" class='offset-md-1 d-flex justify-content-center align-items-center'>
+          <b-card-body :title='roomDetail.name' class='p-2 text-center d-flex justify-content-center align-items-center' style='min-height: 120px; color:white'>
             <b-card-text>
-              <div v-for='player in roomDetail.players'>{{player}}</div>
+              <div v-for='(player, index) in roomDetail.players' :key="index"> <h1>{{player}}</h1></div>
             </b-card-text>
           </b-card-body>
         </b-col>
@@ -31,5 +31,9 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style scoped>
+
+.card {
+  opacity: 0.7;
+}
 </style>
